cfc8c4127dc4f9b33e5a8977a774050e0bf0395b
Poopybuthole69 <Martijn@192.168.2.46>
2021-05-06 15:01:31 +0200

    zlciwf

--- src/main/java/net/mcreator/explodation/item/BackpackItem.java
+++ src/main/java/net/mcreator/explodation/item/BackpackItem.java
@@ -1,15 +1,56 @@
 
 package net.mcreator.explodation.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.fml.network.NetworkHooks;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Direction;
+import net.minecraft.util.ActionResult;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.explodation.gui.BackbackGuiWindow;
+import net.mcreator.explodation.gui.BackbackGui;
+import net.mcreator.explodation.ExplodationModElements;
+
+import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
+
+import io.netty.buffer.Unpooled;
+
 @ExplodationModElements.ModElement.Tag
 public class BackpackItem extends ExplodationModElements.ModElement {
-
 	@ObjectHolder("explodation:backpack")
 	public static final Item block = null;
-
 	public BackpackItem(ExplodationModElements instance) {
 		super(instance, 6);
-
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
@@ -27,9 +68,7 @@ public class BackpackItem extends ExplodationModElements.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(ItemGroup.TOOLS).maxStackSize(1).rarity(Rarity.COMMON));
 			setRegistryName("backpack");
@@ -57,10 +96,8 @@ public class BackpackItem extends ExplodationModElements.ModElement {
 			double x = entity.getPosX();
 			double y = entity.getPosY();
 			double z = entity.getPosZ();
-
 			if (entity instanceof ServerPlayerEntity) {
 				NetworkHooks.openGui((ServerPlayerEntity) entity, new INamedContainerProvider() {
-
 					@Override
 					public ITextComponent getDisplayName() {
 						return new StringTextComponent("Blue Backpack");
@@ -73,13 +110,11 @@ public class BackpackItem extends ExplodationModElements.ModElement {
 						packetBuffer.writeByte(hand == Hand.MAIN_HAND ? 0 : 1);
 						return new BackbackGui.GuiContainerMod(id, inventory, packetBuffer);
 					}
-
 				}, buf -> {
 					buf.writeBlockPos(new BlockPos(x, y, z));
 					buf.writeByte(hand == Hand.MAIN_HAND ? 0 : 1);
 				});
 			}
-
 			return ar;
 		}
 
@@ -104,13 +139,10 @@ public class BackpackItem extends ExplodationModElements.ModElement {
 				stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)
 						.ifPresent(capability -> ((ItemStackHandler) capability).deserializeNBT((CompoundNBT) nbt.get("Inventory")));
 		}
-
 	}
 
 	private static class InventoryCapability implements ICapabilitySerializable<CompoundNBT> {
-
 		private final LazyOptional<ItemStackHandler> inventory = LazyOptional.of(this::createItemHandler);
-
 		@Override
 		public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable Direction side) {
 			return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY ? this.inventory.cast() : LazyOptional.empty();
@@ -128,7 +160,6 @@ public class BackpackItem extends ExplodationModElements.ModElement {
 
 		private ItemStackHandler createItemHandler() {
 			return new ItemStackHandler(27) {
-
 				@Override
 				public int getSlotLimit(int slot) {
 					return 64;
@@ -138,14 +169,11 @@ public class BackpackItem extends ExplodationModElements.ModElement {
 				public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
 					return stack.getItem() != block;
 				}
-
 			};
 		}
 
 		private ItemStackHandler getItemHandler() {
 			return inventory.orElseThrow(RuntimeException::new);
 		}
-
 	}
-
 }
