fcfbbaf20ad62154f8636842907c70c59c300edc
HarmKraats <harmk@192.168.2.47>
2021-05-06 15:08:25 +0200

    beckpeck

---
+++ src/main/java/net/mcreator/explodation/procedures/BackpackKeybindOnKeyPressedProcedure.java
@@ -0,0 +1,80 @@
+package net.mcreator.explodation.procedures;
+
+import net.minecraftforge.fml.network.NetworkHooks;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.explodation.gui.BackbackGui;
+import net.mcreator.explodation.ExplodationModElements;
+import net.mcreator.explodation.ExplodationMod;
+
+import java.util.Map;
+
+import io.netty.buffer.Unpooled;
+
+@ExplodationModElements.ModElement.Tag
+public class BackpackKeybindOnKeyPressedProcedure extends ExplodationModElements.ModElement {
+	public BackpackKeybindOnKeyPressedProcedure(ExplodationModElements instance) {
+		super(instance, 8);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				ExplodationMod.LOGGER.warn("Failed to load dependency entity for procedure BackpackKeybindOnKeyPressed!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				ExplodationMod.LOGGER.warn("Failed to load dependency x for procedure BackpackKeybindOnKeyPressed!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				ExplodationMod.LOGGER.warn("Failed to load dependency y for procedure BackpackKeybindOnKeyPressed!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				ExplodationMod.LOGGER.warn("Failed to load dependency z for procedure BackpackKeybindOnKeyPressed!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				ExplodationMod.LOGGER.warn("Failed to load dependency world for procedure BackpackKeybindOnKeyPressed!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		{
+			Entity _ent = entity;
+			if (_ent instanceof ServerPlayerEntity) {
+				BlockPos _bpos = new BlockPos((int) x, (int) y, (int) z);
+				NetworkHooks.openGui((ServerPlayerEntity) _ent, new INamedContainerProvider() {
+					@Override
+					public ITextComponent getDisplayName() {
+						return new StringTextComponent("Backback");
+					}
+
+					@Override
+					public Container createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
+						return new BackbackGui.GuiContainerMod(id, inventory, new PacketBuffer(Unpooled.buffer()).writeBlockPos(_bpos));
+					}
+				}, _bpos);
+			}
+		}
+	}
+}
